682170e68a5e3f906005d06892957491465c8dbb
Adam Staveley <adam.staveley@energyweb.org>
2020-12-09 09:43:29 +0100

    bump seed for fresh token uid state

--- src/data/tokens.ts
+++ src/data/tokens.ts
@@ -21,7 +21,7 @@ import { extractMSP } from "../tools/tools"
 
 const msp = extractMSP(config.msp.roles)
 
-const mt = MersenneTwister19937.seed(1)
+const mt = MersenneTwister19937.seed(2)
 
 export const tokens: IToken[] = []
 
